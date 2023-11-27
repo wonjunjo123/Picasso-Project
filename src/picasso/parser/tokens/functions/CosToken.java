2aa8bec26c33daf2462435824d430870fd995ecf
Nick Lagges <nlagges@mail.wlu.edu>
2023-11-27 11:48:53 -0500

    Cos token added to picasso.parser.tokens.functions

---
+++ src/picasso/parser/tokens/functions/CosToken.java
@@ -0,0 +1,13 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the cos function token
+ * 
+ */
+public class CosToken extends FunctionToken {
+
+	public CosToken() {
+		super("Cos Function Token");
+	}
+
+}
