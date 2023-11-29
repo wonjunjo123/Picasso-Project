5ce7bc87eb457dd8ac63536126b0fb84e302ee10
wonjunjo <wjo@mail.wlu.edu>
2023-11-28 23:55:51 -0500

    Copied Mark's code to make text box appear.

--- src/picasso/view/Frame.java
+++ src/picasso/view/Frame.java
@@ -2,9 +2,14 @@ package picasso.view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.*;
 
 import picasso.model.Pixmap;
+import picasso.parser.ExpressionTreeGenerator;
 import picasso.util.ThreadedCommand;
 import picasso.view.commands.*;
 
@@ -16,22 +21,38 @@ import picasso.view.commands.*;
  */
 @SuppressWarnings("serial")
 public class Frame extends JFrame {
+	private static JTextField expressionTextField;
+	private JButton validateButton;
 	public Frame(Dimension size) {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		// create GUI components
 		Canvas canvas = new Canvas(this);
 		canvas.setSize(size);
-
+		
+		
 		// add commands to test here
+		
 		ButtonPanel commands = new ButtonPanel(canvas);
+		expressionTextField = new JTextField(40);
+		
 		commands.add("Open", new Reader());
 		commands.add("Evaluate", new ThreadedCommand<Pixmap>(canvas, new Evaluator()));
 		commands.add("Save", new Writer());
+		commands.add(expressionTextField);
+		
+		
+	
+		
+		
 
 		// add our container to Frame and show it
 		getContentPane().add(canvas, BorderLayout.CENTER);
 		getContentPane().add(commands, BorderLayout.NORTH);
 		pack();
+	}
+	
+	public static String getExpressionText() {
+		return expressionTextField.getText();
 	}
 }
