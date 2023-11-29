9463a689526e8218809915cc6bc8e383eef5e499
wonjunjo <wjo@mail.wlu.edu>
2023-11-28 22:57:59 -0500

    Overrode evaluate function to return RGB values based on clamping.

--- src/picasso/parser/language/expressions/Clamp.java
+++ src/picasso/parser/language/expressions/Clamp.java
@@ -3,7 +3,7 @@ package picasso.parser.language.expressions;
 import picasso.parser.language.ExpressionTreeNode;
 
 /**
- * Represents the floor function in the Picasso language.
+ * Represents the clamp function in the Picasso language.
  * 
  * @author Robert C. Duvall
  * @author Wonjun Jo
@@ -24,14 +24,29 @@ public class Clamp extends UnaryFunction {
 	 * Evaluates this expression at the given x,y point by evaluating the clamp of
 	 * the function's parameter.
 	 * 
-	 * @return the color from evaluating the floor of the expression's parameter
+	 * @return the color from evaluating the clamp of the expression's parameter
 	 */
 	@Override
 	public RGBColor evaluate(double x, double y) {
 		RGBColor result = param.evaluate(x, y);
-		double red = Math.floor(result.getRed());
-		double green = Math.floor(result.getGreen());
-		double blue = Math.floor(result.getBlue());
+		double red = result.getRed();
+		double green = result.getGreen();
+		double blue = result.getBlue();
+		if (red > 1) {
+			red = 1;
+		} else if (red < -1) {
+			red = -1;
+		}
+		if (green > 1) {
+			green = 1;
+		} else if (green < -1) {
+			green = -1;
+		}
+		if (blue > 1) {
+			blue = 1;
+		} else if (blue < -1) {
+			blue = -1;
+		}
 
 		return new RGBColor(red, green, blue);
 	}
