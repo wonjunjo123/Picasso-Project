2bad266238173113c09598dd4741bbf73f0a85bc
wonjunjo <wjo@mail.wlu.edu>
2023-11-28 22:58:31 -0500

    Overrode generateExpressionTree to return a Clamp expression tree node.

--- src/picasso/parser/ClampAnalyzer.java
+++ src/picasso/parser/ClampAnalyzer.java
@@ -3,11 +3,11 @@ package picasso.parser;
 import java.util.Stack;
 
 import picasso.parser.language.ExpressionTreeNode;
-import picasso.parser.language.expressions.Floor;
+import picasso.parser.language.expressions.Clamp;
 import picasso.parser.tokens.Token;
 
 /**
- * Handles parsing the floor function.
+ * Handles parsing the clamp function.
  * 
  * @author Wonjun Jo
  * 
@@ -16,12 +16,12 @@ public class ClampAnalyzer extends UnaryFunctionAnalyzer {
 
 	@Override
 	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
-		tokens.pop(); // Need to remove the floor token
+		tokens.pop(); // Need to remove the clamp token
 		// the parameter is the next token(s) on the stack.
 		// But, it needs to be processed
 		ExpressionTreeNode paramETN = SemanticAnalyzer.getInstance().generateExpressionTree(
 				tokens);
-		return new Floor(paramETN);
+		return new Clamp(paramETN);
 	}
 
 }
