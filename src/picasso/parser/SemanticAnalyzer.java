f69f2a467fce71bd2dd57bbaae2780f3be980806
wonjunjo <wjo@mail.wlu.edu>
2023-12-07 16:19:21 -0500

    Added mapping for ImageWrapToken

--- src/picasso/parser/SemanticAnalyzer.java
+++ src/picasso/parser/SemanticAnalyzer.java
@@ -65,6 +65,12 @@ public class SemanticAnalyzer implements SemanticAnalyzerInterface {
 		tokenName = TOKENS_PACKAGE_NAME + "ColorToken";
 		parserName = PARSER_PACKAGE + "ColorAnalyzer";
 		addSemanticAnalyzerMapping(tokenName, parserName);
+		
+		// Image mapping
+		tokenName = TOKENS_PACKAGE_NAME + "ImageWrapToken";
+		parserName = PARSER_PACKAGE + "ImageWrapAnalyzer";
+		addSemanticAnalyzerMapping(tokenName, parserName);
+		
 
 		// TODO: Are there any others that should be added?
 		// Is there a better way to create this mapping?
