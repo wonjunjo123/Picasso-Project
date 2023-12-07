f32115eb840bed8c38e170020a0582ecb8419d0a
wonjunjo <wjo@mail.wlu.edu>
2023-12-07 16:18:46 -0500

    Created ImageWrapToken class

---
+++ src/picasso/parser/tokens/functions/ImageWrapToken.java
@@ -0,0 +1,13 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the ImageWrap function token
+ * 
+ */
+public class ImageWrapToken extends FunctionToken {
+
+	public ImageWrapToken() {
+		super("ImageWrap Function Token");
+	}
+
+}
