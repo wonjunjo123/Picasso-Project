57246d33f2021c031eefff4681fc4d0f2f42520e
wonjunjo <wjo@mail.wlu.edu>
2023-12-08 03:16:34 -0500

    Changed code to return Image ETN

--- src/picasso/parser/ImageAnalyzer.java
+++ src/picasso/parser/ImageAnalyzer.java
@@ -6,6 +6,7 @@ import picasso.parser.language.ExpressionTreeNode;
 import picasso.parser.language.expressions.ImageWrap;
 import picasso.parser.language.expressions.Image;
 import picasso.parser.tokens.Token;
+import picasso.parser.tokens.ImageToken;
 
 /**
  * Handles parsing the Image function.
@@ -16,25 +17,14 @@ import picasso.parser.tokens.Token;
  */
 public class ImageAnalyzer implements SemanticAnalyzerInterface {
 	
-	// NOT THE CODE
-	
-	
-	
-	
-	
 	@Override
 	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
-		tokens.pop(); // Remove the Image token
+		ImageToken imageToken = (ImageToken) tokens.pop(); // Remove the Image token
 		// the parameters are the next tokens on the stack.
 		// But, they need to be processed
 		
-		ExpressionTreeNode yETN = SemanticAnalyzer.getInstance().generateExpressionTree(
-				tokens);
-		ExpressionTreeNode xETN = SemanticAnalyzer.getInstance().generateExpressionTree(
-				tokens);
-		Image imageETN = (Image) SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
-		return new ImageWrap(imageETN, xETN, yETN);
+		String filename = imageToken.getImageName();
+		return new Image(filename);
 	}
-	
 
 }
