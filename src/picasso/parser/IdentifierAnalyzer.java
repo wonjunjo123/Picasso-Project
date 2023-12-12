14f633dc538f4922809ca888592910c467896cba
Nick Lagges <nlagges@mail.wlu.edu>
2023-12-11 23:27:33 -0500

    beginnings of string analysis

--- src/picasso/parser/IdentifierAnalyzer.java
+++ src/picasso/parser/IdentifierAnalyzer.java
@@ -41,14 +41,26 @@ public class IdentifierAnalyzer implements SemanticAnalyzerInterface {
 	 */
 	@Override
 	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
-		IdentifierToken t = (IdentifierToken) tokens.pop();
+		Token token = tokens.pop();
+		IdentifierToken t = (IdentifierToken) token;
 		String id = t.getName();
 		ExpressionTreeNode mapped = idToExpression.get(id);
 		
 		if (mapped != null) {
 			return mapped;
 		}
+		System.out.println("test");
 		if (mapped == null) {
+			String word = token.toString();
+			word = word.substring(word.indexOf(": ")+2, word.length());
+			System.out.println(word);
+			String[] letters = new String[word.length()];
+			int i = 0;
+			while (i < word.length()) {
+				letters[i] = word.substring(i, i+1);
+				i++;
+				System.out.println(i);
+			}
 			String message = "Unrecognized Identifier Variable: " + id;
 			throw new ParseException(message);
 		}
