6a571cfb9e862b5f8e21266559410d2e2e049908
wonjunjo <wjo@mail.wlu.edu>
2023-12-13 18:48:03 -0500

    Added ImageClipToken

---
+++ src/picasso/parser/tokens/functions/ImageClipToken.java
@@ -0,0 +1,13 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the ImageClip function token
+ * 
+ */
+public class ImageClipToken extends FunctionToken {
+
+	public ImageClipToken() {
+		super("ImageClip Function Token");
+	}
+
+}
